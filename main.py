befdfc82cda1a5c6d166a3aadd0c2ec05db97be2
BrunoBenunes <brunobenuness@hotmail.com>
2023-11-14 16:21:08 -0300

    feat(Client.create(), Bank.add_client(). Criando Metodos para criação de um novo usuário e adicionando ele a lista de contas do Banco

--- main.py
+++ main.py
@@ -2,6 +2,34 @@
 
 from abc import ABC, abstractmethod
 
+class Bank:
+    ''' Classe Bank, possui dados relacionado ao banco, como clients e contas cadastradas'''
+    def __init__(self, name) -> None:
+        self.accounts = []
+        self.clients = []
+        self.branchs = []
+        self.name = name
+
+    def verify_account(self, account):
+        ''' Metodo que verifica se uma conta é do banco'''
+        return account in self.accounts
+
+    def verify_client(self, client_cpf):
+        ''' Metodo que verifica se o cliente é do banco'''
+        return client_cpf in self.clients
+
+    def verify_branch(self, branch):
+        ''' Metodo que verifica se a agencia é do banco'''
+        return branch in self.branchs
+
+    def add_client(self, client):
+        ''' Adcionando o client especificado a lista de accounts'''
+        if self.verify_client(client.cpf):
+            print(f'@@@@ O cliente com o CPF : {client.cpf} já Existe! @@@@')
+
+
+
+
 
 class Person(ABC):
     ''' Classe pessoa, contendo informaçes sobre a pessoa'''
@@ -19,6 +47,22 @@ class Client(Person):
     def __init__(self, name, cpf, age, address):
         super().__init__(name, cpf, age, address)
         self.accounts = []
+
+    @classmethod
+    def create(cls, bank : Bank):
+        ''' Metodo para cirar um novo cliente e adicionar ao Banco especificado'''
+        name = input('Digite seu nome e sobrenome: ')
+        cpf = input('Digite seu CPF sem pontuação: ')
+        age = input('Digite sua Idade: ')
+        street = input('Digite a rua/av e o número da casa/complemento: ').title()
+        district = input('Digite o seu Bairro: ')
+        state = input('Digite o UF: *MG*').upper()
+        address = f"{street} - {district}/{state}"
+        new_client = cls(name=name, cpf=cpf, age=age, address=address)
+        bank.add_client(new_client)
+        return new_client
+
+
 
 
 class Account(ABC):
@@ -60,23 +104,32 @@ class SavingAccount(Account):
     def withdraw(self, value):
         self.balance -= value
 
+def login_menu(bank : Bank):
+    ''' Menu De Login do Banco informado '''
+    while True:
+        menu_display = f'''
+    _______________________________________________________
+            $$$ Bem vindo ao Banco {bank.name} $$$
 
-class Bank:
-    ''' Classe Bank, possui dados relacionado ao banco, como clients e contas cadastradas'''
-    def __init__(self, name) -> None:
-        self.accounts = []
-        self.clients = []
-        self.branchs = []
-        self.name = name
+        [e] Entrar
+        [c] Cadastrar
+        [q] Sair
 
-    def verify_account(self, account):
-        ''' Metodo que verifica se uma conta é do banco'''
-        return account in self.accounts
+    _______________________________________________________
 
-    def verify_client(self, client_cpf):
-        ''' Metodo que verifica se o cliente é do banco'''
-        return client_cpf in self.clients
+    '''
+        command = input(menu_display).lower()
 
-    def verify_branch(self, branch):
-        ''' Metodo que verifica se a agencia é do banco'''
-        return branch in self.branchs
+        if command == 'q':
+            print('################# Tenha Um Ótimo Dia! #################')
+
+        if command == 'c':
+            Client.create(Bank)
+            continue
+
+        if command == 'e':
+            login_cpf = input('Digite seu CPF: ')
+
+            if bank.verify_client(login_cpf):
+                # todo client_menu()
+                break # Retirar após client_menu() estiver pronto
